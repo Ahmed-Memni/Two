be2c67257829a24544fc3e3989563064bcedd1e6
ahmed <memniahmed20@gmail.com>
2025-03-05 00:57:25 +0100

    Initial commit

---
+++ app/src/main/java/com/example/twoactivities/SecoundActivity.java
@@ -0,0 +1,38 @@
+package com.example.twoactivities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SecoundActivity extends AppCompatActivity {
+   public static final String EXTRA_REPLY = "key.for.extra.REPLY";
+   private EditText mReply;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_secound);
+        mReply = findViewById(R.id.editText_secound);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void returnReply(View view) {
+        String reply = mReply.getText().toString();
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY,reply);
+        setResult(RESULT_OK,replyIntent);
+        finish();
+    }
+}
